c68ae36cc867026fd2414f3fc904b4b582885091
HORKimhab <kimhab.zeecode@gmail.com>
2024-03-23 22:33:46 +0700

    init: :tada: First commit nestjs crash course

---
+++ nestjs01/README.md
@@ -0,0 +1,41 @@
+# nestjs-01
+
+## Resource
+
+- Project Name: `nestjs-01`
+
+- NestJS Crash Course: https://www.youtube.com/watch?v=xzu3QXwo1BU&list=PL_cUvD4qzbkw-phjGK2qq0nQiG6gw1cKK
+- Learn Next: https://youtu.be/xzu3QXwo1BU?list=PL_cUvD4qzbkw-phjGK2qq0nQiG6gw1cKK&t=1970
+- Test
+
+## Installation
+
+```bash
+$ yarn install
+```
+
+## Running the app
+
+```bash
+# development
+$ yarn run start
+
+# watch mode
+$ yarn run start:dev
+
+# production mode
+$ yarn run start:prod
+```
+
+## Test
+
+```bash
+# unit tests
+$ yarn run test
+
+# e2e tests
+$ yarn run test:e2e
+
+# test coverage
+$ yarn run test:cov
+```
---
+++ nestjs01/src/users/controllers/users/users.controller.ts
@@ -0,0 +1,62 @@
+import { Body, Controller, Get, Param, Post, Req, Res } from '@nestjs/common';
+import { Request, Response, response } from 'express';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+
+@Controller('users')
+export class UsersController {
+
+    @Get()
+    getUsers(){
+        return {username: 'HKimahb', email: 'kimhab.zeecode@gmail.com'};
+    }
+
+    @Get('posts')
+    getUsersPosts(){
+        return [
+            {
+                username: 'HKimhab', 
+                email: 'kimhab.zeecode@gmail.com', 
+                posts: [
+                    {
+                        id: 23, 
+                        title: 'Posts1',
+                    }
+                ]
+            }
+        ]
+    }
+
+    @Post('create')
+    // createUser(@Req() request: Request, @Res() response: Response){
+    //     console.log(request.body);
+    //     response.send('Created');        
+    // }
+    createUser(@Body() userData: CreateUserDto){
+        console.log(userData);
+        return {}
+    }
+
+    @Get(':id/:postId')
+    // getUserById(@Req() request: Request, @Res() response: Response){
+    //     console.log(request.params);
+    //     response.send('');
+    // }
+    getUserById(@Param('id') id: string, @Param('postId') postId: string){
+        console.log(id, postId); 
+        return { id, postId };
+    }
+
+    @Get('posts/comments')
+    getUsersPostsComments(){
+        return [
+          {
+            posts: [
+                {
+                    id: 23, 
+                    title: 'Posts1',
+                }
+            ]
+          }
+        ]
+    }
+}
