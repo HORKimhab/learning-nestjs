36525d7daef77d2794ab1aa2df97c8e656d1c4ae
HORKimhab <kimhab.zeecode@gmail.com>
2024-03-28 11:21:45 +0700

    feat: :boom: Create postUser

--- nestjs02/src/typeorm/entities/User.ts
+++ nestjs02/src/typeorm/entities/User.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Profile } from "./Profile";
+import { Post } from "./Post";
 
 @Entity({ name: 'users' })
 export class User {
@@ -22,5 +23,8 @@ export class User {
     @OneToOne(() => Profile)
     @JoinColumn()
     profile: Profile;
+
+    @OneToMany(() => Post, (post) => post.user)
+    posts: Post[];
 
 }
