4382970929a6d1d0a3b5abe11f4255b4498498b6
HORKimhab <kimhab.zeecode@gmail.com>
2024-03-30 20:07:27 +0700

    feat: :test_tube: Init manual data for test

---
+++ nestjs03/src/__mocks__/mockUsers.ts
@@ -0,0 +1,22 @@
+export const mockUsers = [
+  {
+    id: 1,
+    username: 'HKimhab',
+    displayName: 'HKimhab Dev',
+  },
+  {
+    id: 2,
+    username: 'DSreykhuoch',
+    displayName: 'DSreykhuoch Manager Accountant',
+  },
+  {
+    id: 3,
+    username: 'HKimhab2',
+    displayName: 'HKimhab Dev3',
+  },
+  {
+    id: 4,
+    username: 'HKimhab4',
+    displayName: 'HKimhab 4Dev',
+  },
+];
