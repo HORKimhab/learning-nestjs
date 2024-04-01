b286211abe6e84bb7632028b7697fd85fae18a68
HORKimhab <kimhab.zeecode@gmail.com>
2024-04-01 22:04:15 +0700

    lesson: :memo: Register typeorm and Merge graphql with typeorm

--- nestjs03/src/app.module.ts
+++ nestjs03/src/app.module.ts
@@ -1,14 +1,29 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserSettingsResolver } from './graphsql/resolvers/UserSettingsResolver';
 import { UserResolver } from './graphsql/resolvers/UserResolver';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { User } from './graphsql/models/User';
+import { UserSetting } from './graphsql/models/UserSetting';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.ggl',
+    }),
+    // Register TypeOrmModule 
+    TypeOrmModule.forRoot({
+      type: 'mysql', 
+      host: 'localhost', 
+      port: 3306, 
+      username: 'root',
+      password: '', 
+      database: 'nestjs_mysql_tutorial_nestjs03', 
+      // entities: [User, Profile, Post], 
+      entities: [User, UserSetting], 
+      synchronize: true,
     }),
   ],
   controllers: [],
--- nestjs03/src/graphsql/models/User.ts
+++ nestjs03/src/graphsql/models/User.ts
@@ -1,17 +1,25 @@
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { UserSetting } from './UserSetting';
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 
+@Entity({ name: 'users' })
 @ObjectType()
 export class User {
+
+  @PrimaryGeneratedColumn()
   @Field(() => Int)
   id: number;
 
+  @Column()
   @Field()
   username: string;
 
+  @Column()
   @Field({ nullable: true })
   displayName?: string;
 
+  @OneToOne(() => UserSetting)
+  @JoinColumn()
   @Field({ nullable: true })
   settings?: UserSetting;
 }
