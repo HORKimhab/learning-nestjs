efcf7519f527d468a89a885823d6e491d6da2531
HORKimhab <kimhab.zeecode@gmail.com>
2024-04-20 23:50:27 +0700

    lesson: :memo: Init session

--- nestjs01-tutorial/src/auth/controllers/auth/auth.controller.ts
+++ nestjs01-tutorial/src/auth/controllers/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, Session, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
@@ -7,5 +7,11 @@ export class AuthController {
     @Post('login')
     async login(@Request() req){
 
+    }
+
+    @Get('')
+    async getAuthSession(@Session() session: Record<string, any>){
+       session.authenticated = true; 
+       return session; 
     }
 }
--- nestjs01-tutorial/src/main.ts
+++ nestjs01-tutorial/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from "passport"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.setGlobalPrefix('api');
+  app.use(
+    session({
+      name: 'NESTJS_SESSION_ID',
+      secret: 'ABCDKENNKEKALEKXEKSKEKKXSLEJAAJELLE9EIRIR9284903SNEN',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 60000, 
+      }
+    }),
+  );
+  app.use(passport.initialize())
+  app.use(passport.session())
   await app.listen(3000);
+
 }
 bootstrap();
