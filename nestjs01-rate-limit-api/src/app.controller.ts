c2086ca16738de71b502fb085320d35a86548f74
HORKimhab <kimhab.zeecode@gmail.com>
2024-04-21 21:31:30 +0700

    feat: :memo: Init Rate limit api

--- nestjs01-rate-limit-api/src/app.controller.ts
+++ nestjs01-rate-limit-api/src/app.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SkipThrottle, Throttle } from '@nestjs/throttler';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // For specific endpoint
+  @SkipThrottle()
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // (30000 ms = 30 seconds * 1000 ms/second).
+  @Throttle({ default: { limit: 3, ttl: 3000 } })
+  @Get('users')
+  getUsers() {
+    return [];
+  }
+
+  @Throttle({ default: { limit: 3, ttl: 3000 } })
+  @Get('users/posts')
+  getUsersPosts() {
+    return {};
   }
 }
--- nestjs01-rate-limit-api/src/app.module.ts
+++ nestjs01-rate-limit-api/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    // Rate limit api for globally
+    ThrottlerModule.forRoot([
+      { limit: 3, ttl: 60000 }
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService, 
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ],
 })
 export class AppModule {}
